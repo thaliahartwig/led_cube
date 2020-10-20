caed533258778ad9553c465bcc664ca6315295a8
Thalia Hartwig <hartwig.thalia@gmail.com>
2020-10-19 20:01:42 -0400

    setup file

---
+++ led_cube.ino
@@ -0,0 +1,53 @@
+/*
+  Description: This is a self-driven project to act as an introduction to microcontrollers, as well as
+  physical computing. This project use the 4x4x4 led cube project created by KyleTheCreater on instructable 
+  as a reference. The link to this reference can be found on my git page under the description for this 
+  project. (https://www.instructables.com/4x4x4-LED-Cube-Arduino-Uno/)
+  
+  This project also serves as a the means to obtain skills in soldering for the creation of the cube which,
+  I would have learnt throught University, had there been no restrictions put in place as a result of Covid-19.
+
+
+  Creator: Thalia Hartwig
+  Hardware: Arduino UNO (generic)
+  Software: Visual Studio Code --> Arduino Extension
+  Date Created: October 19, 2020
+  Version: Version 0.0
+  Last Revision: October 19, 2020
+*/
+
+/*
+  Configuration: A right-handed axis of reference is used for this project
+                                  z 
+                                  |
+                                  |
+                                  |
+                                  |
+                                  |
+                                  |_______________________ y
+                                 /*  *  *  *
+                                /*  *  *  *
+                               /*  *  *  *
+                              /*  *  *  *
+                             /
+                            /
+                           x 
+              (above model displays the first layer; z = 0)
+*/
+
+//declaring local variables
+
+// anodes --> layers
+
+// cathodes --> columns
+
+// built-in functions required
+
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
